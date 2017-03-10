91b9183869ce3cd9dfd6796f524fc7b07362b285
Anne Graham <anagramfx@gmail.com>
2017-03-10 14:41:54 -0800

    refactor mongoose code and add POST todos route - works with postman

--- server/server.js
+++ server/server.js
@@ -1,58 +1,27 @@
-const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var express = require('express');
 
-// prefers Promises. designate which lib
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// create a model - a todo model - add some validation
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+// middleware
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// new model User
-// email required and trimmed and min length of 1
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
-});
-
-var dude = new User({
-  email: '   myemail@mydomain.com '
-});
-
-dude.save().then((doc) => {
-  console.log('Saved user ', JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save user', e);
+app.listen(3000, () => {
+  console.log('started on port 3000');
 })
-
-// how to create an instance:
-
-// var now = Date.now();
-// var newTodo = new Todo({
-//  text: 'save a dude in users collection'
-//
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo ', JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('Unable to save Todo', e);
-// });
