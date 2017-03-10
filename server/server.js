bcb3cfcf6771fe4cf4292247995c2716b9be9a9c
Anne Graham <anagramfx@gmail.com>
2017-03-10 13:24:00 -0800

    make a new model user

--- server/server.js
+++ server/server.js
@@ -4,29 +4,51 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// create a model - a todo model -
+// create a model - a todo model - add some validation
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-// how to create an instance:
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-var now = Date.now();
-var newTodo = new Todo({
+// new model User
+// email required and trimmed and min length of 1
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
 
-  text: 'number as completedAt a bit later',
-  completed: true,
-  completedAt: now
+var dude = new User({
+  email: 'dude@excellent.com'
+});
+
+dude.save().then((doc) => {
+  console.log('Saved user ', JSON.stringify(doc, undefined, 2));
+}, (e) => {
+  console.log('Unable to save user', e);
+})
+
+// how to create an instance:
+
+// var now = Date.now();
+var newTodo = new Todo({
+  text: 'save a dude in users collection'
+
 });
 
 newTodo.save().then((doc) => {
