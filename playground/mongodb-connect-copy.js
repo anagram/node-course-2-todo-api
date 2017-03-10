3969b93c34aaba00c4a82f6b154fbd2a1be2e7c6
Anne Graham <anagramfx@gmail.com>
2017-03-09 16:33:50 -0800

    add playground

---
+++ playground/mongodb-connect-copy.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+ var obj = new ObjectID();
+ console.log(obj);
+// use object de-structuring in ES6
+var user = {name: 'han', age: 42};
+var {name, age} = user;
+console.log(`name var pulled out: ${name} and age: ${age}`);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   test: 'something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Han',
+  //   age: 42,
+  //   location: 'Moons of Aldebaran'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  db.close();
+})
---
+++ playground/mongodb-find.js
@@ -0,0 +1,46 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find().toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58c1e42d009ca34e74900d3a')}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Leia'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+  // db.close();
+})
